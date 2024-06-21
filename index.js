188cd39c2927f5199256526428b7960975ec9e8f
Zuhaitz <zuhaitzmartinezjimenez@gmail.com>
2024-06-21 19:02:04 +0200

    express conection added

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("hello world!");
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+const PORT = 3000;
+
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}...`));
