d7e9e4f09ed70c8bf2f997166053de22f4113821
CarBlank <crblanquer@gmail.com>
2024-06-24 20:14:04 +0200

    category CRUD

--- controllers/CategoryController.js
+++ controllers/CategoryController.js
@@ -1,25 +1,66 @@
-const { Category } = require("../models/index.js");
+const { Category, Sequelize } = require("../models/index.js");
+const { Op } = Sequelize;
+
 const CategoryController = {
   create(req, res) {
-    req.body.role = "category";
     Category.create(req.body)
       .then((category) =>
-        res
-          .status(201)
-          .send({ message: "Categoria creada correctamente", category }),
+        res.status(201).send({ message: "Category was created", category }),
       )
       .catch((err) => console.error(err));
   },
 
   getAll(req, res) {
-    req.body.role = "category";
-    Category.getAll(req.body)
+    Category.findAll()
       .then((category) =>
         res
           .status(201)
-          .send({ message: "Categoria traidas correctamente", category }),
+          .send({ message: "These are all the categories", category }),
       )
       .catch((err) => console.error(err));
+  },
+
+  getById(req, res) {
+    Category.findByPk(req.params.id)
+      .then((category) => res.status(200).json(category))
+      .catch((error) => {
+        console.log(error);
+        res.status(500).json({ error: error.message });
+      });
+  },
+
+  async delete(req, res) {
+    try {
+      await Category.destroy({
+        where: { id: req.params.id },
+      });
+      res.send({ message: "The category has been removed" });
+    } catch (error) {
+      console.log(error);
+    }
+  },
+
+  async update(req, res) {
+    try {
+      await Category.update(req.body, {
+        where: { id: req.params.id },
+      });
+      const category = await Category.findByPk(req.params.id);
+      res.send("Category update");
+    } catch (error) {
+      console.error(error);
+      res.status(500).send({ message: "Is not possible update the category" });
+    }
+  },
+
+  getOneByName(req, res) {
+    Category.findOne({
+      where: {
+        name: {
+          [Op.like]: `%${req.params.name}%`,
+        },
+      },
+    }).then((category) => res.send(category));
   },
 };
 
--- routes/category.js
+++ routes/category.js
@@ -3,5 +3,10 @@ const router = express.Router();
 const CategoryController = require("../controllers/CategoryController");
 
 router.post("/", CategoryController.create);
+router.get("/", CategoryController.getAll);
+router.delete("/:id", CategoryController.delete);
+router.put("/:id", CategoryController.update);
+router.get("/:id", CategoryController.getById);
+router.get("/name/:name", CategoryController.getOneByName);
 
 module.exports = router;
