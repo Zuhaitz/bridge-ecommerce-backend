0b5e8e078e85503b7f15a2734b49aee3abf93293
Zuhaitz Martínez <zuhaitzmartinezjimenez@gmail.com>
2024-08-30 20:04:47 +0200

    added cors to api

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ const { typeError } = require("./middleware/errors");
 const app = express();
 const PORT = 3000;
 
+const cors = require("cors");
+const corsOptions = {
+  origin: "http://localhost:5173",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+
 app.use(express.json());
 app.use("/users", require("./routes/users"));
 app.use("/categories", require("./routes/categories"));
